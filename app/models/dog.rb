c0ce055ff681303b729c7bba72cf423ba2f1e188
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-13 20:03:36 +0000

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,12 +4,15 @@ class Dog
 
   @@all = []
 
-  def initialize
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
     @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
 end
